7ce75b76e8bebd22ea9f2c4b97a79c32e53fdb22
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2019-02-14 10:25:06 +0100

    Added test setup with tox

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="data-uploader",
+    version="0.0.1",
+    author="Origo Dataplattform",
+    author_email="dataplattform@oslo.kommune.no",
+    description="Returns a presigned URL and form fields that can be used to POST a file to S3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.oslo.kommune.no/origo-dataplatform/data-uploader",
+    packages=setuptools.find_packages(),
+    install_requires=['jsonschema==2.6.0']
+)
