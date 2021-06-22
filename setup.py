7aa647873725786bd091f902d85df989e83d88ca
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2021-06-22 13:07:25 +0200

    DP-1681 Post-migration fixes after moving to github.com

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-    name="data-uploader",
+    name="okdata-data-uploader",
     version="0.0.1",
     author="Origo Dataplattform",
     author_email="dataplattform@oslo.kommune.no",
     description="Returns a presigned URL and form fields that can be used to POST a file to S3",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.oslo.kommune.no/origo-dataplatform/data-uploader",
+    url="https://github.com/oslokommune/okdata-data-uploader",
     packages=setuptools.find_packages(),
     install_requires=[
         "boto3",
